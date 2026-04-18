1adef881d5eae194024a5631e84aec50b2d819d0
Bart Römgens <bart.romgens@gmail.com>
2026-04-18 15:47:48 +0200

    Make sidenav persistent on large screens

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
@@ -22,5 +25,19 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class App {
   protected readonly title = 'Caraser';
-  protected sidenavOpened = false;
+
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  protected readonly isHandset = toSignal(
+    this.breakpointObserver.observe('(max-width: 959.98px)').pipe(map(r => r.matches)),
+    { initialValue: false }
+  );
+
+  protected readonly sidenavOpened = signal(false);
+
+  protected closeOnMobile(): void {
+    if (this.isHandset()) {
+      this.sidenavOpened.set(false);
+    }
+  }
 }
